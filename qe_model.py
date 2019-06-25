1b515d15e7e793f6d68a64f308bf3d84bd392c69
joerischasfoort <joerischasfoort@gmail.com>
2019-06-25 18:15:23 +0200

    add qe shading to graph

--- qe_model.py
+++ qe_model.py
@@ -19,6 +19,7 @@ def qe_model(traders, central_bank, orderbook, parameters, scenario=None, seed=1
 
     fundamentals = [parameters["fundamental_value"]]
 
+    orderbook.qe_periods = []
     orderbook.tick_close_price.append(fundamentals[-1])
 
     traders_by_wealth = [t for t in traders]
@@ -108,9 +109,11 @@ def qe_model(traders, central_bank, orderbook, parameters, scenario=None, seed=1
             if cb_demand > 0:
                 bid = orderbook.add_bid(orderbook.lowest_ask_price, cb_demand, central_bank)
                 central_bank.var.active_orders.append(bid)
+                orderbook.qe_periods.append(qe_tick)
             elif cb_demand < 0:
                 ask = orderbook.add_ask(orderbook.highest_bid_price, -cb_demand, central_bank)
                 central_bank.var.active_orders.append(ask)
+                orderbook.qe_periods.append(qe_tick)
 
             # END QE ##############################################################################################
             # select random sample of active traders
